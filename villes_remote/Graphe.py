0574374bc59dae36f5b017e32eced80acafeb759
Aarys Sahler <aarys.sahler@gmail.com>
2025-03-26 15:39:53 +0100

    error fix

--- villes_remote/Graphe.py
+++ villes_remote/Graphe.py
@@ -149,10 +149,10 @@ class Graphe:
             if x_max is None or x > x_max:
                 x_max = x 
 
-            if y_min is None or y > y_min:
+            if y_min is None or y < y_min:
                 y_min = y 
             
-            if y_max is None or y < y_max:
+            if y_max is None or y > y_max:
                 y_max = y 
 
         return x_min, x_max, y_min, y_max
@@ -177,12 +177,13 @@ if __name__ == "__main__":
     graphe.ajouter_ville(Ville("Paris", 48.85661, 2.35222, 48))
     graphe.ajouter_ville(Ville("Nancy", 48.69096039092552, 6.185302734375001, 205))
     graphe.ajouter_ville(Ville("Strasbourg", 48.574789910928864, 7.756347656250001, 146))
-    graphe.ajouter_ville(Ville("Rennes", 48.11476663187632, -1.6918945312500002, 30))
-    graphe.ajouter_ville(Ville("Belfort", 47.62097541515849, 6.844482421875001, 368))
-    graphe.ajouter_ville(Ville("Poitiers", 46.589903823511385, 0.3291457762100203, 124))
+    # graphe.ajouter_ville(Ville("Rennes", 48.11476663187632, -1.6918945312500002, 30))
+    # graphe.ajouter_ville(Ville("Belfort", 47.62097541515849, 6.844482421875001, 368))
+    # graphe.ajouter_ville(Ville("Poitiers", 46.589903823511385, 0.3291457762100203, 124))
     graphe.ajouter_ville(Ville("Lyon", 45.69214056550071, 4.812714357262654, 176))
-    graphe.ajouter_ville(Ville("Limoges", 45.69214056550071, 1.2522334252502578, 296))
+    # graphe.ajouter_ville(Ville("Limoges", 45.69214056550071, 1.2522334252502578, 296))
 
     graphe.creation_aretes()
 
     print(graphe.carte)
+    print(graphe.distance_moyenne())
